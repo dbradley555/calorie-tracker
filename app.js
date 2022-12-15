a36a2b1f0fd648dd3ef331310c60e862640d857c
dbradley555 <dbradley5555@gmail.com>
2022-12-15 09:09:31 -0600

    added files

---
+++ app.js
@@ -0,0 +1,125 @@
+// Storage Controller
+
+// Item Controller
+const ItemCtrl = (function () {
+  const Item = function (id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  };
+  const data = {
+    items: [
+      // { id: 0, name: 'Steak Dinner', calories: 1200 },
+      // { id: 1, name: 'Cookie', calories: 500 },
+      // { id: 2, name: 'Eggs', calories: 400 },
+    ],
+    currentItem: null,
+    totalCalories: 0,
+  };
+  return {
+    getItems: function () {
+      return data.items;
+    },
+    addItem: function (name, calories) {
+      let ID;
+      if (data.items.length > 0) {
+        ID = data.items[data.items.length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+      calories = parseInt(calories);
+      newItem = new Item(ID, name, calories);
+      data.items.push(newItem);
+      return newItem;
+    },
+    logData: function () {
+      return data;
+    },
+  };
+})();
+
+// UI Controller
+const UICtrl = (function () {
+  const UISelectors = {
+    itemList: '#item-list',
+    addBtn: '.add-btn',
+    itemNameInput: '#item-name',
+    itemCaloriesInput: '#item-calories',
+  };
+  return {
+    populateItemList: function (items) {
+      let html = '';
+      items.forEach(function (item) {
+        html += `<li class="collection-item" id="item-${item.id}">
+            <strong>${item.name}:</strong> <em>${item.calories} calories</em>
+            <a href="#" class="secondary-content"
+              ><i class="material-icons">create</i></a
+            >
+          </li>`;
+      });
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    },
+    getItemInput: function () {
+      return {
+        name: document.querySelector(UISelectors.itemNameInput).value,
+        calories: document.querySelector(UISelectors.itemCaloriesInput).value,
+      };
+    },
+    addListItem: function (item) {
+      document.querySelector(UISelectors.itemList).style.display = 'block';
+      const li = document.createElement('li');
+      li.className = 'collection-item';
+      li.id = `item-${item.id}`;
+      li.innerHTML = `<strong>${item.name}:</strong> <em>${item.calories} calories</em>
+      <a href="#" class="secondary-content"
+        ><i class="material-icons">create</i></a
+      >`;
+      document
+        .querySelector(UISelectors.itemList)
+        .insertAdjacentElement('beforeend', li);
+    },
+    clearInput: function () {
+      document.querySelector(UISelectors.itemNameInput).value = '';
+      document.querySelector(UISelectors.itemCaloriesInput).value = '';
+    },
+    hideList: function () {
+      document.querySelector(UISelectors.itemList).style.display = 'none';
+    },
+    getSelectors: function () {
+      return UISelectors;
+    },
+  };
+})();
+
+// App Controller
+const App = (function (ItemCtrl, UICtrl) {
+  const loadEventListeners = function () {
+    const UISelectors = UICtrl.getSelectors();
+    document
+      .querySelector(UISelectors.addBtn)
+      .addEventListener('click', itemAddSubmit);
+  };
+  const itemAddSubmit = function (e) {
+    const input = UICtrl.getItemInput();
+    if (input.name !== '' && input.calories !== '') {
+      const newItem = ItemCtrl.addItem(input.name, input.calories);
+      UICtrl.addListItem(newItem);
+      UICtrl.clearInput();
+    }
+    e.preventDefault();
+  };
+  return {
+    init: function () {
+      const items = ItemCtrl.getItems();
+      if (items.length === 0) {
+        UICtrl.hideList();
+      } else {
+        UICtrl.populateItemList(items);
+        loadEventListeners();
+      }
+    },
+  };
+})(ItemCtrl, UICtrl);
+
+// Initialize App
+App.init();
